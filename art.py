3abb54e9be6a1f79e5d1380bdcc210277d97b762
Jinook Jung <jinook929@gmail.com>
2021-04-10 01:56:25 +0000

    Initial commit

---
+++ art.py
@@ -0,0 +1,9 @@
+logo = '''
+Days of Month Calculator
+
+       ,dP""d8b,
+      d"   d88"8b
+     I8    Y88a88)
+     `Y, a  )888P
+       "b,,a88P"
+'''
---
+++ main.py
@@ -0,0 +1,68 @@
+from art import logo
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        print("Leap year.")
+        return True
+      else:
+        print("Not leap year.")
+        return False
+    else:
+      print("Leap year.")
+      return True
+  else:
+    print("Not leap year.")
+    return False
+
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  month_index = int(month) - 1
+  if is_leap(int(year)):
+    month_days[1] = 29
+  return month_days[month_index]
+  
+print(logo)
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+import unittest
+
+class MyTest(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(days_in_month(2018, 2), 28)
+
+    def test_2(self):
+        self.assertEqual(days_in_month(2020, 2), 29)
+
+    def test_3(self):
+        self.assertEqual(days_in_month(2019, 4), 30)
+
+    def test_4(self):
+        self.assertEqual(days_in_month(1045, 5), 31)
+
+print("\n")
+print('Running some tests on your code:')
+print(".\n.\n.\n.")
+unittest.main(verbosity=1, exit=False)
